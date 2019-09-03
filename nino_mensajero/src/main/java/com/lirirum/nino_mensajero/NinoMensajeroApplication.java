4929c2a7331699a398cf4fdeb855ecd03f87a7d4
lurianne <luriannemarianne@gmail.com>
2019-09-03 09:25:51 -0400

    Initial commit

---
+++ nino_mensajero/src/main/java/com/lirirum/nino_mensajero/NinoMensajeroApplication.java
@@ -0,0 +1,13 @@
+package com.lirirum.nino_mensajero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NinoMensajeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NinoMensajeroApplication.class, args);
+	}
+
+}
---
+++ nino_mensajero/src/test/java/com/lirirum/nino_mensajero/NinoMensajeroApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lirirum.nino_mensajero;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NinoMensajeroApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
