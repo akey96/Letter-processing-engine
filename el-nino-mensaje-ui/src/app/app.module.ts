6d7551bf702f57edbf6885369437f13c2d2c52a4
Roxana Quispe <afr_roxi@hotmail.com>
2019-09-24 10:16:52 +0000

    Update app.module.ts

--- el-nino-mensaje-ui/src/app/app.module.ts
+++ el-nino-mensaje-ui/src/app/app.module.ts
@@ -2,13 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PersonalComponent } from './personal/personal.component';
+
+
+const routes: Routes = [
+
+  { path: 'personal', component: PersonalComponent},
+
+  
+];
 
 @NgModule({
   declarations: [
     AppComponent
+        PersonalComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+        RouterModule.forRoot(routes),
+
   ],
   providers: [],
   bootstrap: [AppComponent]
