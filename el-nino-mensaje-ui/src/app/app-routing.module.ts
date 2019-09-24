b0bf2a552b11de4f9b89f521135353aeb12a5298
Gabriela Torrico <gabriela.nataly.torico@gmail.com>
2019-09-24 10:12:09 -0400

    creacion de formulario base

--- el-nino-mensaje-ui/src/app/app-routing.module.ts
+++ el-nino-mensaje-ui/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { WriteLetterComponent } from './home/write-letter/write-letter.component';
+import { RegisterUserComponent } from './register-user/register-user.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'write-letter',  pathMatch: 'full' },
-  { path: 'write-letter', component: WriteLetterComponent}
+  { path: 'write-letter', component: WriteLetterComponent},
+  { path: 'register-user', component: RegisterUserComponent}
 ];
 
 @NgModule({
