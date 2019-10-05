0321bd03358d5422bdc578540a74af5096b87fb4
Fernando Soto <39037895+K4waiiChan@users.noreply.github.com>
2019-10-04 22:57:00 -0400

    fixes on register users

--- nino_mensajero/src/main/java/com/lirirum/nino_mensajero/user/Person.java
+++ nino_mensajero/src/main/java/com/lirirum/nino_mensajero/user/Person.java
@@ -15,4 +15,21 @@ public class Person {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private long id;
+    @NotBlank
+    @NotNull
+    private String name;
+    @NotNull
+    @NotBlank
+    @Column(unique = true, nullable = false)
+    private String email;
+    @Past
+    private Date birthday;
+    @NotNull
+    private String username;
+    @NotNull
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private PersonRole userRole;
+    @Enumerated(EnumType.STRING)
+    private PersonStatus userStatus;
 }
