eebbc369e2d5adc132b5f7037bf4a832827cde7c
lurianne <luriannemarianne@gmail.com>
2019-11-08 14:38:38 -0400

    corenlp settings and pipeline addition

---
+++ nino_mensajero/src/main/java/com/lirirum/nino_mensajero/Utils/TextMotor/Pipeline.java
@@ -0,0 +1,24 @@
+package com.lirirum.nino_mensajero.Utils.TextMotor;
+
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.util.StringUtils;
+
+import java.util.Properties;
+
+public class Pipeline {
+
+    private final static Properties properties;
+    private final static String annotatorsNames="tokenize, ssplit, pos";
+    private final static String langProps="StanfordCoreNLP-spanish.properties";
+    private static StanfordCoreNLP stanfordCoreNLP;
+
+    static {
+        properties = StringUtils.argsToProperties(new String[]{"-annotators", annotatorsNames,"-props",langProps});
+    }
+
+    public static  StanfordCoreNLP getPipeline(){
+        if(stanfordCoreNLP == null)
+            stanfordCoreNLP = new StanfordCoreNLP(properties);
+        return stanfordCoreNLP;
+    }
+}
