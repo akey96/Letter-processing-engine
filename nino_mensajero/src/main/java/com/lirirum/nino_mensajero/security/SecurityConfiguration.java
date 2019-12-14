acc4ba4811a9fadcea6b92990ad71bced958156c
Fernando Soto <39037895+K4waiiChan@users.noreply.github.com>
2019-12-14 02:42:02 -0400

    administrator now is added once the program start

--- nino_mensajero/src/main/java/com/lirirum/nino_mensajero/security/SecurityConfiguration.java
+++ nino_mensajero/src/main/java/com/lirirum/nino_mensajero/security/SecurityConfiguration.java
@@ -53,7 +53,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
-                .cors().and()
+                .cors().disable()
                 .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/oauth/token").permitAll()
