169a1d40b55e0994a859743d99a7e3b9bc852200
Fernando Soto <39037895+K4waiiChan@users.noreply.github.com>
2019-12-16 00:13:20 -0400

    person id now is exposed

--- nino_mensajero/src/main/java/com/lirirum/nino_mensajero/GlobalRepositoryRestConfigurer.java
+++ nino_mensajero/src/main/java/com/lirirum/nino_mensajero/GlobalRepositoryRestConfigurer.java
@@ -2,6 +2,7 @@ package com.lirirum.nino_mensajero;
 
 import com.lirirum.nino_mensajero.content.Content;
 import com.lirirum.nino_mensajero.letter.Letter;
+import com.lirirum.nino_mensajero.user.Person;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
@@ -14,7 +15,7 @@ public class GlobalRepositoryRestConfigurer extends RepositoryRestConfigurerAdap
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
-        config.exposeIdsFor(Letter.class, Content.class);
+        config.exposeIdsFor(Letter.class, Content.class, Person.class);
     }
 
 }
