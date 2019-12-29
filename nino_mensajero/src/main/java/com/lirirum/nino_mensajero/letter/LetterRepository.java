cec9ef337015510db14cf9c9880cd722b7ab5d9b
Fernando Soto <39037895+K4waiiChan@users.noreply.github.com>
2019-12-29 14:14:53 -0400

    correction on letter repository

--- nino_mensajero/src/main/java/com/lirirum/nino_mensajero/letter/LetterRepository.java
+++ nino_mensajero/src/main/java/com/lirirum/nino_mensajero/letter/LetterRepository.java
@@ -4,12 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
-import java.util.Optional;
+
 
 @RepositoryRestResource
 public interface LetterRepository extends JpaRepository<Letter, Long> {
-    Optional<List<Letter>> findByContentId(Long contentId);
-
-    Optional<List<Letter>> findByResponsableId(Long responsableId);
+    List<Letter> findByContentId(Long contentId);
+    List<Letter> findByResponsableId(Long responsableId);
 
 }
